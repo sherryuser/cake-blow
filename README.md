994488306300e3e48ba3c7914ee5dc1252f5fb8c
sherryuser <sherryuser1@gmail.com>
2023-12-19 17:36:26 +0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+# 🎂 Interactive Birthday Cake Project 🎉
+
+## Overview
+
+This Interactive Birthday Cake 🍰 project is a delightful web-based application hosted on GitHub Pages. Users can interactively add candles 🕯️ to a birthday cake and blow them out using their microphone, combining HTML, CSS, and JavaScript for a charming user experience.
+
+## Motivation
+
+Inspired by a creative Instagram post ([View Post](https://www.instagram.com/reel/C0xoHgDLmqg/)), this project brings the joy of celebrating with a virtual cake to your screen.
+
+## Credits
+
+The cake design was adapted from Fazlur Rahman's creation on CodePen ([View Original Design](https://codepen.io/fazlurr/pen/gPMJMK)).
+
+## Features
+
+- **Interactive Cake**: Click to add candles 🕯️.
+- **Microphone Integration**: Blow into your mic to extinguish candles 🎤.
+- **Candle Count**: Watch the candle count change dynamically 🔢.
+- **Engaging UI**: Engage with a user-friendly interface.
+
+## Accessing the Project
+
+To experience the Interactive Birthday Cake, visit the GitHub Pages link: [Interactive Birthday Cake](#)
+
+## How to Use
+
+1. Go to the provided GitHub Pages link.
+2. Click on the cake to add candles.
+3. Allow microphone access and blow into your microphone to blow out the candles.
+
+## Technologies
+
+- HTML
+- CSS
+- JavaScript
+
+## License
+
+This project is released under the MIT License. This means it can be used, modified, and distributed freely, as long as it is not used for commercial purposes.
---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cake = document.querySelector(".cake");
+  const candleCountDisplay = document.getElementById("candleCount");
+  let candles = [];
+  let audioContext;
+  let analyser;
+  let microphone;
+
+  function updateCandleCount() {
+    const activeCandles = candles.filter(
+      (candle) => !candle.classList.contains("out")
+    ).length;
+    candleCountDisplay.textContent = activeCandles;
+  }
+
+  function addCandle(left, top) {
+    const candle = document.createElement("div");
+    candle.className = "candle";
+    candle.style.left = left + "px";
+    candle.style.top = top + "px";
+
+    const flame = document.createElement("div");
+    flame.className = "flame";
+    candle.appendChild(flame);
+
+    cake.appendChild(candle);
+    candles.push(candle);
+    updateCandleCount();
+  }
+
+  cake.addEventListener("click", function (event) {
+    const rect = cake.getBoundingClientRect();
+    const left = event.clientX - rect.left;
+    const top = event.clientY - rect.top;
+    addCandle(left, top);
+  });
+
+  function isBlowing() {
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+
+    let sum = 0;
+    for (let i = 0; i < bufferLength; i++) {
+      sum += dataArray[i];
+    }
+    let average = sum / bufferLength;
+
+    return average > 40; //
+  }
+
+  function blowOutCandles() {
+    let blownOut = 0;
+
+    if (isBlowing()) {
+      candles.forEach((candle) => {
+        if (!candle.classList.contains("out") && Math.random() > 0.5) {
+          candle.classList.add("out");
+          blownOut++;
+        }
+      });
+    }
+
+    if (blownOut > 0) {
+      updateCandleCount();
+    }
+  }
+
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(function (stream) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = audioContext.createAnalyser();
+        microphone = audioContext.createMediaStreamSource(stream);
+        microphone.connect(analyser);
+        analyser.fftSize = 256;
+        setInterval(blowOutCandles, 200);
+      })
+      .catch(function (err) {
+        console.log("Unable to access microphone: " + err);
+      });
+  } else {
+    console.log("getUserMedia not supported on your browser!");
+  }
+});
